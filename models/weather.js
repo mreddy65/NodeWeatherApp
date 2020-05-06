74d16c37aab3860d990aa9cbb97606b409333a9b
Maggie Reddy <Maggie.Reddy@castandcrew.com>
2020-05-06 13:18:54 -0700

    removes comments

--- models/weather.js
+++ models/weather.js
@@ -46,24 +46,5 @@ var weatherSchema = new Schema({
     temperature: [{type: Number}]  
 });
 
-// const weatherSchema = new weatherSchema(
-//     {
-//          "id": 1,
-//          "date": "1985-01-01",
-//          "location": {
-//          "lat": 35.1442,
-//          "lon": -111.6664,
-//          "city": "Flagstaff",
-//          "state": "Arizona"
-//          },
-//          "temperature": [
-//          28.5, 27.6, 26.7, 25.9, 25.3, 24.7,
-//          24.3, 24.0, 27.1, 34.0, 38.6, 41.3,
-//          43.2, 44.4, 45.0, 45.3, 45.1, 44.2,
-//          41.9, 38.0, 35.0, 33.0, 31.1, 29.9
-//          ]
-//       }
-//     );
-
     const weatherModel = mongoose.model('weather', weatherSchema);
     module.exports= weatherModel;
